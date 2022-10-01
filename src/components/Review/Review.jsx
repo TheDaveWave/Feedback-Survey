587f7cd1a910ebcfaa3b959f2df16c64c2720683
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-01 15:39:08 -0500

    added display of feedback on review page

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,7 +1,18 @@
+import Header from "../Header/Header";
+import { useSelector } from 'react-redux';
+
 function Review() {
+    // get the redux store reducer's state.
+    const feedback = useSelector(store => store.feedbackReducer);
+
     return (
         <section>
-            <h1>Review</h1>
+            <h1>Review Your Feedback</h1>
+            <p>Feeling: {feedback.feeling}</p>
+            <p>Understanding: {feedback.understanding}</p>
+            <p>Support: {feedback.supported}</p>
+            <p>Comments: {feedback.comments}</p>
+            <button>Submit</button>
         </section>
     );
 }
