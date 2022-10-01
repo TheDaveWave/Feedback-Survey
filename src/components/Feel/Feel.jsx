7b9684d7be9812bdb6d262419a093b7d9b69b010
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-01 14:27:59 -0500

    created the first mockup of the first part of the form

--- src/components/Feel/Feel.jsx
+++ src/components/Feel/Feel.jsx
@@ -4,7 +4,16 @@ function Feel() {
     return (
         <section>
             <Header />
-            <h1>Feeling?</h1>
+            <h1>How are you feeling today?</h1>
+            <label htmlFor='feeling-select'>Feeling?</label>
+            {/* Creating a select element so there will always be a value and limited range */}
+            <select id='feeling-select' name='feeling'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+            </select>
         </section>
     );
 }
