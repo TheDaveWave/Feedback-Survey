9456099427f354215e311bf27cc310a7fe1af93c
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-01 16:56:21 -0500

    created successful post request on server side

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -6,15 +6,34 @@ const pool = require('../modules/pool');
 // setup get request.
 router.get('/', (req, res) => {
     // set up the sql query text.
-    const queryText = 'SELECT * FROM "feedback" ORDER BY "date", "id";';
+    const queryText = 'SELECT * FROM "feedback" ORDER BY "date" DESC, "id";';
     pool.query(queryText)
     .then((response) => {
         res.send(response.rows);
     })
     .catch(err => {
-        console.log('Error getting feedback');
+        console.log('Error getting feedback', err);
         res.sendStatus(500);
+    });
+});
+
+// POST request to /feedback.
+router.post('/', (req, res) => {
+    console.log('In POST /feedback',req.body);
+    const par = req.body;
+    // sanitzing the data in the query text.
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4);`;
+    // setting up the values to put into the query.
+    const values = [par.feeling, par.understanding, par.support, par.comments];
+    pool.query(queryText, values)
+    .then(() => {
+        res.sendStatus(201);
     })
+    .catch(err => {
+        console.log('Error in POST /feedback', err);
+        res.sendStatus(500);
+    });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
+app.use(express.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
