21873e38f362623230af35bfc7e036e1ab4da358
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-01 15:54:59 -0500

    added success component

---
+++ src/components/Success/Success.jsx
@@ -0,0 +1,10 @@
+function Success() {
+    return (
+        <section>
+            <h1>Form Submitted!</h1>
+            <button>Leave New Feedback</button>
+        </section>
+    );
+}
+
+export default Success;
