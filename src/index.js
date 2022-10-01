0751dcfa40a7ea6a770f1e275f869795ce689e0d
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-01 12:26:25 -0500

    ran npm i redux react-redux

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
