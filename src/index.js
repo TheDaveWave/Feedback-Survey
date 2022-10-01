4efdaa3a96f73995fa9e6c03f593b76b4064a5c3
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-01 13:40:35 -0500

    created a feedback reducer and store instance

--- src/index.js
+++ src/index.js
@@ -2,6 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create a default object to store the information from the client feedback forms into redux.
+const feedbackInfo = {
+    feeling: 0,
+    understanding: 0,
+    supported: 0,
+    comments: '',
+}
+
+// reducer for feedback forms, default state is equal to the previously defined object.
+const feedbackReducer = (state = feedbackInfo, action) => {
+    // use a switch statement to detemine what to do with state and action per action.type
+    switch (action.type) {
+        case 'ADD_FEELING':
+            
+            break;
+        case 'ADD_UNDERSTANDING':
+
+            break;
+        case 'ADD_SUPPORTED':
+
+            break;
+        case 'ADD_COMMENTS':
+
+            break;
+        default:
+            return state;
+    }
+}
+
+const storeInstance = createStore(
+    combineReducers(
+        feedbackReducer
+    ),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(
+    //  set a store instance for accessing data.
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
