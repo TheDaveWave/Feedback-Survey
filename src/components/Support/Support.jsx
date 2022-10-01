aa64df115a902401513d8c201a677e23b323ecaa
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-01 14:32:57 -0500

    repeated the code for feel.jsx and implemented it into understand.jsx and support.jsx

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,7 +1,19 @@
+import Header from "../Header/Header";
+
 function Support() {
     return (
         <section>
-            <h1>Support?</h1>
+            <Header />
+            <h1>How well are you being supported?</h1>
+            <label htmlFor='support-select'>Feeling?</label>
+            {/* Creating a select element so there will always be a value and limited range */}
+            <select id='support-select' name='support'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+            </select>
         </section>
     );
 }
--- src/components/Understand/Understand.jsx
+++ src/components/Understand/Understand.jsx
@@ -1,7 +1,19 @@
+import Header from "../Header/Header";
+
 function Understand() {
     return (
         <section>
-            <h1>Understand?</h1>
+            <Header />
+            <h1>How well are you understanding the content?</h1>
+            <label htmlFor='understand-select'>Understanding?</label>
+            {/* Creating a select element so there will always be a value and limited range */}
+            <select id='understand-select' name='understand'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+            </select>
         </section>
     );
 }
