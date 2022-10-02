8a7e3cd6087506eddb1f3fc30b0456aad694b4a4
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-02 11:56:18 -0500

    displayed feedback on admin page

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Admin() {
+    // set a local state to store the data from the database.
+    const [feedbackData, setFeedbackData] = useState([]);
+    // GET request to receive feedback data from the database.
+    const fetchFeedback = () => {
+        // axios GET request.
+        axios.get('/feedback')
+        .then(response => {
+            // console.log(response.data);
+            // set the received data to feedbackData in the local state.
+            setFeedbackData(response.data);
+        })
+        .catch(err => {
+            console.log('Error getting feedback in Admin', err);
+        });
+    }
+
+    // call fetchFeedback on load and only once every load of page.
+    useEffect(() => {
+        fetchFeedback();
+    }, []);
+
     return (
         <section>
             <h1>Feedback Results!</h1>
@@ -13,7 +37,16 @@ function Admin() {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {/* loop over feedbackData and display each feedback objects keys on the DOM */}
+                    {feedbackData.map(feedback => (
+                        <tr key={feedback.id}>
+                            <td>{feedback.feeling}</td>
+                            <td>{feedback.understanding}</td>
+                            <td>{feedback.support}</td>
+                            <td>{feedback.comments}</td>
+                            <td><button>Delete</button></td>
+                        </tr>
+                    ))}
                 </tbody>
                 <tfoot></tfoot>
             </table>
