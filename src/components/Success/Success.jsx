4de1d075ad279eed8c884d7d4bfb28bffcf1153a
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-02 14:34:03 -0500

    completed success page

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Success.css';
 // just a little bit of fun here.
 function Success() {
+
+    const history = useHistory();
+    // learned how to make it so you can not go back to other pages.
+    const noBack = () => {
+        window.addEventListener('popstate', evt => (
+            history.replace(null)
+        ));
+    }
+
+    useEffect(() => {
+        noBack();
+    },[]);
+
     return (
         <main id='success-page'></main>
     );
