09d817af99facca93ddab21d43334903ec04a60f
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-02 13:28:09 -0500

    created another sweetalert for submitting feedback

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
+import swal from 'sweetalert';
 
 function Review() {
     // get the redux store reducer's state.
@@ -8,21 +9,42 @@ function Review() {
     const history = useHistory();
 
     const handleClick = () => {
-        // post route to server.
-        axios.post('/feedback', {
-            feeling: feedback.feeling,
-            understanding: feedback.understanding,
-            support: feedback.support,
-            comments: feedback.comments
+        // use sweetalert to confirm submission.
+        swal({
+            title: 'Submit?',
+            text: 'Once submitted you will not be able to edit feedback.',
+            buttons: {
+                cancel: true,
+                confirm: 'Submit'
+            }
         })
-        .then((result) => {
-            // console.log(result.data);
-        })
-        .catch(err => {
-            console.log('Error in axios /feedback', err);
+        .then(ok => {
+            if(ok) {
+                // post route to server.
+                axios.post('/feedback', {
+                    feeling: feedback.feeling,
+                    understanding: feedback.understanding,
+                    support: feedback.support,
+                    comments: feedback.comments
+                })
+                .then((result) => {
+                    // console.log(result.data);
+                })
+                .catch(err => {
+                    console.log('Error in axios /feedback', err);
+                });
+                // have a cool success pop up appear.
+                swal({
+                    icon: 'success'
+                })
+                .then(value => {
+                    if(value) {
+                        // push to next page.
+                        history.push('/success');
+                    }
+                });
+            }
         });
-        // push to next page.
-        history.push('/success');
     }
 
     return (
